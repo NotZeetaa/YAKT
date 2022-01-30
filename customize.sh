4237da6e0ab8133a7bd61aedbe8934aa118b465e
akirasupr <vishal.rockstar7011@gmail.com>
2022-01-31 03:54:22 +0530

    rename install.sh to customize.sh
    update customize script

---
+++ customize.sh
@@ -0,0 +1,30 @@
+set -x
+SKIPUNZIP=1
+MODLOG=/data/ZTS
+RM_RF() {
+rm -rf $MODLOG 2>/dev/null
+rm -rf $MODPATH/LICENSE 2>/dev/null
+rm -rf $MODPATH/README.md 2>/dev/null
+}
+SET_PERMISSION() {
+set_perm_recursive $MODPATH 0 0 0755 0644
+set_perm_recursive $MODPATH/script 0 0 0755 0700
+}
+MOD_EXTRACT() {
+ui_print "- Extracting module files"
+unzip -o "$ZIPFILE" 'script/*' -d $MODPATH >&2
+unzip -o "$ZIPFILE" service.sh -d $MODPATH >&2
+unzip -o "$ZIPFILE" module.prop -d $MODPATH >&2
+}
+MOD_PRINT() {
+ui_print ""
+ui_print "*************************************"
+ui_print " Zeetaa Tweaks Module Rebase V1. "
+ui_print " Thx to lybdroid for his module template.      "
+ui_print "*************************************"
+ui_print ""
+}
+RM_RF
+MOD_PRINT
+MOD_EXTRACT
+SET_PERMISSION
