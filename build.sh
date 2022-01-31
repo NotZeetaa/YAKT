06fc35cacfc5f16a4c7000e34f827408c5d79d2c
akirasupr <vishal.rockstar7011@gmail.com>
2022-01-31 05:36:56 +0530

    build: update
    add changelog.txt

--- build.sh
+++ build.sh
@@ -1,21 +1,17 @@
-#!/bin/sh
-
-rm -rf *.zip
-
+#!/usr/bin/env bash
 token=5073803429:AAFx8cf00fzcwtGrcDI-HfOcHviIZhNr30s
-chat_id=-1001234010295
-
-
-zip -r9 Zeetaa-Tweaks-Rebase-BETA.zip *
-
-ZIP=Zeetaa-Tweaks-Rebase-BETA.zip
-
-curl -F document=@$ZIP "https://api.telegram.org/bot$token/sendDocument" \
-     -F chat_id="$chat_id" 
-
-curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
-        -d chat_id="$chat_id" \
-        -d "disable_web_page_preview=true" \
-        -d "parse_mode=html" \
-        -d text="Changelog:%0A- Reduce TCP performance spikes%0A- Introduce Cgroup Boost%0A- Enable ECN negotiation by default%0A"
-      
+chatid=-1001234010295
+clog=`cat changelog.txt`
+function post_file() {
+curl -F document=@$1 "https://api.telegram.org/bot${token}/sendDocument" \
+     -F chat_id="${chatid}"  \
+     -F "disable_web_page_preview=true" \
+     -F "parse_mode=html" \
+     -F caption="${clog}"
+}
+echo ""
+echo -n "Give me version name : "
+read -r Version
+rm -rf Zeetaa-Tweaks-Rebase-*
+zip -r9 "Zeetaa-Tweaks-Rebase-${version}.zip" . -x *build* -x *changelog* -x *.bak* -x *.git*
+post_file "Zeetaa-Tweaks-Rebase-${version}.zip"
