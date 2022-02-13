5c57de0fe1484485855342be53f7ce0f116f9252
NotZeetaa <rodrigo2005contente@gmail.com>
2022-02-14 03:25:29 +0530

    tweak: Clean Up & Update
    * Use sdcard dir instead of DATA
    
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- script/zeetaatweaks.sh
+++ script/zeetaatweaks.sh
@@ -6,95 +6,87 @@ sleep 60
 
 SC=/sys/devices/system/cpu/cpu0/cpufreq/schedutil
 KP=/sys/module/kprofiles
-LOG=/data/ZTS
+LOG=/sdcard/ZeetaaTweaks.log
 TP=/dev/stune/top-app/uclamp.max
 DV=/dev/stune
 CP=/dev/cpuset
 
-# Check if folder exist
-# If not then remove and create a new one
-if [ -d $LOG ]; then
-  rm -rf $LOG
-  mkdir -p $LOG
-else
-  mkdir -p $LOG
-fi
-echo "# ZeetaaTweaks V0.4" > $LOG/log.txt
-echo "# Build Date: 28/01/2022" >> $LOG/log.txt
-echo "# By @NotZeetaa (Github)" >> $LOG/log.txt
-echo " " >> $LOG/log.txt
-echo "$(date "+%H:%M:%S") * Device: $(getprop ro.product.system.model)" >> $LOG/log.txt
-echo "$(date "+%H:%M:%S") * Kernel: $(uname -r)" >> $LOG/log.txt
-echo "$(date "+%H:%M:%S") * Android Version: $(getprop ro.system.build.version.release)" >> $LOG/log.txt
-echo " " >> $LOG/log.txt
+echo "# ZeetaaTweaks V0.4" > $LOG
+echo "# Build Date: 28/01/2022" >> $LOG
+echo "# By @NotZeetaa (Github)" >> $LOG
+echo " " >> $LOG
+echo "$(date "+%H:%M:%S") * Device: $(getprop ro.product.system.model)" >> $LOG
+echo "$(date "+%H:%M:%S") * Kernel: $(uname -r)" >> $LOG
+echo "$(date "+%H:%M:%S") * Android Version: $(getprop ro.system.build.version.release)" >> $LOG
+echo " " >> $LOG
 
 # Use Google's schedutil rate-limits from Pixel 3
 # Credits to Kdrag0n
-echo "$(date "+%H:%M:%S") * Applying Google's schedutil rate-limits from Pixel 3" >> $LOG/log.txt
+echo "$(date "+%H:%M:%S") * Applying Google's schedutil rate-limits from Pixel 3" >> $LOG
 sleep 0.5
 if [ -e $SC ]; then
   echo 500 > $SC/up_rate_limit_us
   echo 20000 > $SC/down_rate_limit_us
-  echo "$(date "+%H:%M:%S") * Applied Google's schedutil rate-limits from Pixel 3" >> $LOG/log.txt
+  echo "$(date "+%H:%M:%S") * Applied Google's schedutil rate-limits from Pixel 3" >> $LOG
 else
-  echo "$(date "+%H:%M:%S") * Abort You are not using schedutil governor" >> $LOG/log.txt
+  echo "$(date "+%H:%M:%S") * Abort You are not using schedutil governor" >> $LOG
 fi
-echo " " >> $LOG/log.txt
+echo " " >> $LOG
   
 # Tweak aims to have less Latency
 # Credits to SpiderMoon and Rhoan
-echo "$(date "+%H:%M:%S") * Tweaking to Reduce Latency [BETA]" >> $LOG/log.txt
+echo "$(date "+%H:%M:%S") * Tweaking to Reduce Latency [BETA]" >> $LOG
 echo 128 > /proc/sys/kernel/sched_nr_migrate
 sleep 0.5
-echo "$(date "+%H:%M:%S") * Done [BETA]" >> $LOG/log.txt
-echo " " >> $LOG/log.txt
+echo "$(date "+%H:%M:%S") * Done [BETA]" >> $LOG
+echo " " >> $LOG
 
 # Kprofiles Tweak
 # Credits to cyberknight
 if [ -d $KP ]; then
-  echo "$(date "+%H:%M:%S") * Your Kernel Supports Kprofiles" >> $LOG/log.txt
+  echo "$(date "+%H:%M:%S") * Your Kernel Supports Kprofiles" >> $LOG
   echo 2 > $KP/parameters/mode
 else
-  echo "$(date "+%H:%M:%S") * Your Kernel doesn't support Kprofiles, not a big trouble, its normal" >> $LOG/log.txt
-  echo " " >> $LOG/log.txt
+  echo "$(date "+%H:%M:%S") * Your Kernel doesn't support Kprofiles, not a big trouble, its normal" >> $LOG
+  echo " " >> $LOG
 fi
 
 # Less Ram Usage
 # The stat_interval one, reduces jitter (Credits to tytydraco)
 # Credits to RedHat for dirty_ratio
-echo "$(date "+%H:%M:%S") * Applying Ram Tweaks" >> $LOG/log.txt
+echo "$(date "+%H:%M:%S") * Applying Ram Tweaks" >> $LOG
 sleep 0.5
 echo 50 > /proc/sys/vm/vfs_cache_pressure
 echo 10 > /proc/sys/vm/stat_interval
 echo 10 > /proc/sys/vm/dirty_ratio
 echo 3 > /proc/sys/vm/dirty_background_ratio
-echo "$(date "+%H:%M:%S") * Applied Ram Tweaks" >> $LOG/log.txt
-echo " " >> $LOG/log.txt
+echo "$(date "+%H:%M:%S") * Applied Ram Tweaks" >> $LOG
+echo " " >> $LOG
 
 # Clean Up Ram
-echo "$(date "+%H:%M:%S") * Cleaning Up Ram" >> $LOG/log.txt
+echo "$(date "+%H:%M:%S") * Cleaning Up Ram" >> $LOG
 echo 3 > /proc/sys/vm/drop_caches
-echo "$(date "+%H:%M:%S") * Done" >> $LOG/log.txt
+echo "$(date "+%H:%M:%S") * Done" >> $LOG
 echo 0 > /proc/sys/vm/drop_caches
-echo " " >> $LOG/log.txt
+echo " " >> $LOG
 
 # Set 15 to perf_cpu_time_max_percent
 echo 15 > /proc/sys/kernel/perf_cpu_time_max_percent
 
 # Disable Timer migration
-echo "$(date "+%H:%M:%S") * Disabling Timer Migration" >> $LOG/log.txt
+echo "$(date "+%H:%M:%S") * Disabling Timer Migration" >> $LOG
 echo "0" > /proc/sys/kernel/timer_migration
-echo "$(date "+%H:%M:%S") * Done" >> $LOG/log.txt
-echo " " >> $LOG/log.txt
+echo "$(date "+%H:%M:%S") * Done" >> $LOG
+echo " " >> $LOG
 
 # Cgroup Boost
-echo "$(date "+%H:%M:%S") * Checking which scheduler has ur kernel" >> $LOG/log.txt
+echo "$(date "+%H:%M:%S") * Checking which scheduler has ur kernel" >> $LOG
 sleep 0.5
 if [ -e $TP ]; then
   # Uclamp Tweaks
   # All credits to @darkhz
-  echo "$(date "+%H:%M:%S") * You have uclamp scheduler" >> $LOG/log.txt
-  echo "$(date "+%H:%M:%S") * Applying tweaks for it" >> $LOG/log.txt
+  echo "$(date "+%H:%M:%S") * You have uclamp scheduler" >> $LOG
+  echo "$(date "+%H:%M:%S") * Applying tweaks for it" >> $LOG
   sleep 0.3
   sysctl -w kernel.sched_util_clamp_min_rt_default=96
   sysctl -w kernel.sched_util_clamp_min=128
@@ -114,19 +106,19 @@ if [ -e $TP ]; then
   echo 0 > $CP/system-background/uclamp.min
   echo 0 > $CP/system-background/uclamp.boosted
   echo 0 > $CP/system-background/uclamp.latency_sensitive
-  echo "$(date "+%H:%M:%S") * Done" >> $LOG/log.txt
-  echo " " >> $LOG/log.txt
+  echo "$(date "+%H:%M:%S") * Done" >> $LOG
+  echo " " >> $LOG
 else
-  echo "$(date "+%H:%M:%S") * You have normal cgroup scheduler" >> $LOG/log.txt
-  echo "$(date "+%H:%M:%S") * Applying tweaks for it" >> $LOG/log.txt
+  echo "$(date "+%H:%M:%S") * You have normal cgroup scheduler" >> $LOG
+  echo "$(date "+%H:%M:%S") * Applying tweaks for it" >> $LOG
   sleep 0.3
   chmod 644 $DV/top-app/schedtune.boost
   echo 5 > $DV/top-app/schedtune.boost
   chmod 664 $DV/top-app/schedtune.boost
   echo 1 > $DV/foreground/schedtune.boost
   echo 0 > $DV/background/schedtune.boost
-  echo "$(date "+%H:%M:%S") * Done" >> $LOG/log.txt
-  echo " " >> $LOG/log.txt
+  echo "$(date "+%H:%M:%S") * Done" >> $LOG
+  echo " " >> $LOG
 fi
 
 # ipv4 tweaks
@@ -141,34 +133,34 @@ echo 1 > /proc/sys/net/ipv4/tcp_ecn
 
 # Always allow sched boosting on top-app tasks
 # Credits to tytydraco
-# echo "$(date "+%H:%M:%S") * Always allow sched boosting on top-app tasks" >> $LOG/log.txt
+# echo "$(date "+%H:%M:%S") * Always allow sched boosting on top-app tasks" >> $LOG
 # echo 0 > /proc/sys/kernel/sched_min_task_util_for_colocation
-# echo "$(date "+%H:%M:%S") * Done" >> $LOG/log.txt
-# echo " " >> $LOG/log.txt
+# echo "$(date "+%H:%M:%S") * Done" >> $LOG
+# echo " " >> $LOG
 
 # Watermark Boost
-echo "$(date "+%H:%M:%S") * Checking if its a 5.4 kernel to apply watermark boost" >> $LOG/log.txt
+echo "$(date "+%H:%M:%S") * Checking if its a 5.4 kernel to apply watermark boost" >> $LOG
 if cat /proc/version | grep -w "5.4"
 then 
   echo "$(date "+%H:%M:%S") * Found 5.4 Kernel applying watermark boost for it"
   echo 1500 > /proc/sys/vm/watermark_boost_factor
-  echo "$(date "+%H:%M:%S") * Done" >> $LOG/log.txt
+  echo "$(date "+%H:%M:%S") * Done" >> $LOG
 else
-  echo "$(date "+%H:%M:%S") * Didn't found watermark boost or its a 4.19 kernel which doesn't work" >> $LOG/log.txt
+  echo "$(date "+%H:%M:%S") * Didn't found watermark boost or its a 4.19 kernel which doesn't work" >> $LOG
   echo 0 > /proc/sys/vm/watermark_boost_factor
 fi
-echo " " >> $LOG/log.txt
+echo " " >> $LOG
 
-echo "$(date "+%H:%M:%S") * Tweaking read_ahead overall" >> $LOG/log.txt
+echo "$(date "+%H:%M:%S") * Tweaking read_ahead overall" >> $LOG
 for queue in /sys/block/*/queue
 do
 echo 128 > "${queue}"/read_ahead_kb
 done
-echo " " >> $LOG/log.txt
+echo " " >> $LOG
 
 # Tune lease-break-time
 echo 5 > /proc/sys/fs/lease-break-time
 
-echo "$(date "+%H:%M:%S") * The Tweak is done enjoy :)" >> $LOG/log.txt
+echo "$(date "+%H:%M:%S") * The Tweak is done enjoy :)" >> $LOG
 
 su -lp 2000 -c "cmd notification post -S bigtext -t Zeetaa-Tweaks Activated"
