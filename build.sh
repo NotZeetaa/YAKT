8da16b9cac5607272fec3994620de49ec5d2f6d9
NotZeetaa <rodrigo2005contente@gmail.com>
2022-02-26 11:35:04 +0000

    build: Update
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- build.sh
+++ build.sh
@@ -9,5 +9,5 @@ curl -F document=@$1 "https://api.telegram.org/bot${token}/sendDocument" \
 }
 echo ""
 rm -rf *.zip
-zip -r9 "Zeetaa-Tweaks-Rebase-BETA.zip" . -x *build* -x *changelog* -x *.bak* -x *.git*
-push "Zeetaa-Tweaks-Rebase-BETA.zip"
+zip -r9 "Zeetaa-Tweaks-Rebase-BETA-V1.4.zip" . -x *build* -x *changelog* -x *.bak* -x *.git*
+push "Zeetaa-Tweaks-Rebase-BETA-V1.4.zip"
