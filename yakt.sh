872884265b492eb9bda9881ddf0a2b20150baf75
NotZeetaa <rodrigo2005contente@gmail.com>
2022-06-08 17:27:40 +0000

    yakt: Clean Up & Optimize
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- yakt.sh
+++ yakt.sh
@@ -12,6 +12,8 @@ DV=/dev/stune
 CP=/dev/cpuset
 MC=/sys/module/mmc_core
 WT=/proc/sys/vm/watermark_boost_factor
+KL=/proc/sys/kernel
+VM=/proc/sys/vm
 
 PS=$(cat /proc/version)
 BT=$(getprop ro.boot.bootdevice)
@@ -44,9 +46,9 @@ echo " " >> $LOG
 # (Rewrited) Tweaks to have less Latency
 # Credits to RedHat & tytydraco
 echo "$(date "+%H:%M:%S") * Tweaking to Reduce Latency " >> $LOG
-echo 15000000 > /proc/sys/kernel/sched_wakeup_granularity_ns
-echo 10000000 > /proc/sys/kernel/sched_min_granularity_ns
-echo 5000000 > /proc/sys/kernel/sched_migration_cost_ns
+echo 15000000 > $KL/sched_wakeup_granularity_ns
+echo 10000000 > $KL/sched_min_granularity_ns
+echo 5000000 > $KL/sched_migration_cost_ns
 sleep 0.5
 echo "$(date "+%H:%M:%S") * Done " >> $LOG
 echo " " >> $LOG
@@ -70,14 +72,14 @@ echo " " >> $LOG
 # Credits to RedHat for dirty_ratio
 echo "$(date "+%H:%M:%S") * Applying Ram Tweaks" >> $LOG
 sleep 0.5
-echo 50 > /proc/sys/vm/vfs_cache_pressure
-echo 10 > /proc/sys/vm/stat_interval
+echo 50 > $VM/vfs_cache_pressure
+echo 10 > $VM/stat_interval
 echo "$(date "+%H:%M:%S") * Applied Ram Tweaks" >> $LOG
 echo " " >> $LOG
 
 # Set kernel.perf_cpu_time_max_percent to 15
 echo "$(date "+%H:%M:%S") * Applying tweak for perf_cpu_time_max_percent" >> $LOG
-echo 15 > /proc/sys/kernel/perf_cpu_time_max_percent
+echo 15 > $KL/perf_cpu_time_max_percent
 echo "$(date "+%H:%M:%S") * Done" >> $LOG
 echo " " >> $LOG
 
@@ -85,10 +87,10 @@ echo " " >> $LOG
 # Also iostats & reduce latency
 # Credits to tytydraco
 echo "$(date "+%H:%M:%S") * Disabling some scheduler logs/stats" >> $LOG
-if [ -e /proc/sys/kernel/sched_schedstats ]; then
-  echo 0 > /proc/sys/kernel/sched_schedstats
+if [ -e $KL/sched_schedstats ]; then
+  echo 0 > $KL/sched_schedstats
 fi
-echo off > /proc/sys/kernel/printk_devkmsg
+echo off > $KL/printk_devkmsg
 for queue in /sys/block/*/queue
 do
     echo 0 > "$queue/iostats"
@@ -99,7 +101,7 @@ echo " " >> $LOG
 
 # Disable Timer migration
 echo "$(date "+%H:%M:%S") * Disabling Timer Migration" >> $LOG
-echo 0 > /proc/sys/kernel/timer_migration
+echo 0 > $KL/timer_migration
 echo "$(date "+%H:%M:%S") * Done" >> $LOG
 echo " " >> $LOG
 
@@ -159,7 +161,7 @@ echo 1 > /proc/sys/net/ipv4/tcp_ecn
 # Always allow sched boosting on top-app tasks
 # Credits to tytydraco
 echo "$(date "+%H:%M:%S") * Always allow sched boosting on top-app tasks" >> $LOG
-echo 0 > /proc/sys/kernel/sched_min_task_util_for_colocation
+echo 0 > $KL/sched_min_task_util_for_colocation
 echo "$(date "+%H:%M:%S") * Done" >> $LOG
 echo " " >> $LOG
 
@@ -168,7 +170,7 @@ echo "$(date "+%H:%M:%S") * Checking if you have watermark boost support" >> $LO
 if [[ "$PS" == *"4.19"* ]]
 then
   echo "$(date "+%H:%M:%S") * Found 4.19 kernel, disabling watermark boost because doesn't work..." >> $LOG
-  echo 0 > /proc/sys/vm/watermark_boost_factor
+  echo 0 > $VM/watermark_boost_factor
   echo "$(date "+%H:%M:%S") * Done!" >> $LOG
 elif [ -e $WT ]; then
   echo "$(date "+%H:%M:%S") * Found Watermark Boost support, tweaking it" >> $LOG
@@ -203,7 +205,7 @@ echo " " >> $LOG
 # Extfrag
 # Credits to @tytydraco
 echo "$(date "+%H:%M:%S") * Increasing fragmentation index" >> $LOG
-echo 750 > /proc/sys/vm/extfrag_threshold
+echo 750 > $VM/extfrag_threshold
 sleep 0.5
 echo "$(date "+%H:%M:%S") * Done!" >> $LOG
 echo " " >> $LOG
