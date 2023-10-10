ccddb1698771322db420932cb09535037a99778a
CRANKV2 <97807862+CRANKV2@users.noreply.github.com>
2023-10-10 20:56:47 +0200

    Update yakt.sh
    Reworked and Improved Script
    
    Description:
    - Added a `write()` function to handle file writes more robustly.
    - Enhanced logging with timestamps for better error tracking.
    - Organized and cleaned up the code for readability.
    - Created a dedicated directory for error log files inside main yakt MODDIR.
    - Improved error handling with informative messages.
    - Simplified file path checking and handling.
    
    These changes make the script more reliable, easier to maintain, and provide better feedback in case of errors. Enjoy the improved YAKT script!
    
    Contributor: @CRANKV2

--- yakt.sh
+++ yakt.sh
@@ -1,19 +1,75 @@
-#!/system/bin/sh
-# Yakt v11
-# Author: @NotZeetaa (Github)
-# ×××××××××××××××××××××××××× #
+#!/system/bin/sh 
+ # Yakt v11
+ # Author: @NotZeetaa (Github)
+ # Contributed: @CRANKV2 (Github)
+  # ×××××××××××××××××××××××××× #
 
-sleep 30
-# Log create
-if [ ! -d /sdcard/Documents ]; then
-    LOG=/sdcard/yakt.log
-else
-    if [ ! -d /sdcard/Documents/yakt ]; then
-        mkdir /sdcard/Documents/yakt
-        LOG=/sdcard/Documents/yakt/yakt.log
-    else
-        LOG=/sdcard/Documents/yakt/yakt.log
+#sleep 30
+# Function to log normal messages
+log-yakt() {
+    local message="$1"
+    echo "[$(date "+%H:%M:%S")] $message" >> $LOG
+}
+
+# Function to log error messages
+log-error() {
+    local message="$1"
+    echo "[$(date "+%H:%M:%S")] $message" >> $ERROR_LOG
+}
+
+# Function to write values to files
+write() {
+    local file="$1"
+    local value="$2"
+
+    # Check if the file exists
+    if [ ! -f "$file" ]; then
+        log-error "Error: File $file does not exist."
+        return 1
     fi
+
+    # Make file writable
+    chmod +w "$file" 2>/dev/null
+
+    # Write new value, bail out if it fails
+    if ! echo "$value" >"$file" 2>/dev/null; then
+        log-error "Error: Failed to write to $file."
+        return 1
+    else
+        return 0
+    fi
+}
+
+# Set the log file paths
+if [ ! -d /sdcard/Documents/yakt ]; then
+    mkdir -p /sdcard/Documents/yakt
+    if [ $? -ne 0 ]; then
+        log-error "Error: Unable to create directory /sdcard/Documents/yakt"
+        exit 1
+    fi
+fi
+
+LOG=/sdcard/Documents/yakt/yakt.log
+ERROR_LOG=/data/adb/modules/YAKT/yakt-logging-error.log
+
+if [ -f "$LOG" ]; then
+    rm "$LOG"
+fi
+
+if [ -f "$ERROR_LOG" ]; then
+    rm "$ERROR_LOG"
+fi
+
+touch "$LOG"
+if [ $? -ne 0 ]; then
+    log-error "Error: Unable to create log file $LOG"
+    exit 1
+fi
+
+touch "$ERROR_LOG"
+if [ $? -ne 0 ]; then
+    log-error "Error: Unable to create error log file $ERROR_LOG"
+    exit 1
 fi
 
 # Variables
@@ -28,194 +84,229 @@ BT=$(getprop ro.boot.bootdevice)
 BL=/dev/blkio
 SCHED_PERIOD="$((1 * 1000 * 1000))"
 
-# Info
-echo "# YAKT v11" > $LOG
-echo "# Build Date: 05/10/2023" >> $LOG
-echo -e "# Author: @NotZeetaa (Github)\n" >> $LOG
-echo "[$(date "+%H:%M:%S")] Device: $(getprop ro.product.system.model)" >> $LOG
-echo "[$(date "+%H:%M:%S")] Brand: $(getprop ro.product.system.brand)" >> $LOG
-echo "[$(date "+%H:%M:%S")] Kernel: $(uname -r)" >> $LOG
-echo "[$(date "+%H:%M:%S")] Rom build type: $(getprop ro.system.build.type)" >> $LOG
-echo -e "[$(date "+%H:%M:%S")] Android Version: $(getprop ro.system.build.version.release)\n" >> $LOG
+
+# Start logging error info
+log-yakt "Starting YAKT v11"
+log-yakt "Build Date: 05/10/2023"
+log-yakt "Author: @NotZeetaa (Github)"
+log-yakt "Device: $(getprop ro.product.system.model)"
+log-yakt "Brand: $(getprop ro.product.system.brand)"
+log-yakt "Kernel: $(uname -r)"
+log-yakt "Rom build type: $(getprop ro.system.build.type)"
+log-yakt "Android Version: $(getprop ro.system.build.version.release)"
 
 # Grouping tasks tweak
-echo "[$(date "+%H:%M:%S")] Enabling Sched Auto Group..." >> $LOG
-echo 1 > $KL/sched_autogroup_enabled
-echo -e "[$(date "+%H:%M:%S")] Done.\n" >> $LOG
+log-yakt ""
+log-yakt "Enabling Sched Auto Group..."
+write "$KL/sched_autogroup_enabled" 1
+log-yakt "Done."
+log-yakt ""
+
 
 # Tweak scheduler to have less Latency
-# Credits to RedHat & tytydraco & KTweak
-echo "[$(date "+%H:%M:%S")] Tweaking to Reduce Latency " >> $LOG
-echo 5000000 > $KL/sched_migration_cost_ns
-echo "$SCHED_PERIOD" > $KL/sched_latency_ns
-echo 16 > $KL/sched_nr_migrate
+log-yakt ""
+log-yakt "Tweaking to Reduce Latency"
+write "$KL/sched_migration_cost_ns" 5000000
+write "$KL/sched_latency_ns" "$SCHED_PERIOD"
+write "$KL/sched_nr_migrate" 16
 sleep 0.5
-echo -e "[$(date "+%H:%M:%S")] Done.\n" >> $LOG
+log-yakt "Done."
+log-yakt ""
+
+
 
 # Enable CRF by default
-echo "[$(date "+%H:%M:%S")] Enabling child_runs_first" >> $LOG
-echo 1 > $KL/sched_child_runs_first
-echo -e "[$(date "+%H:%M:%S")] Done.\n" >> $LOG
+log-yakt ""
+log-yakt "Enabling child_runs_first"
+write "$KL/sched_child_runs_first" 1
+log-yakt "Done."
+log-yakt ""
+
 
 # Ram Tweak
-# The stat_interval one reduces jitter (Credits to kdrag0n)
-# Credits to RedHat for dirty_ratio
-echo "[$(date "+%H:%M:%S")] Applying Ram Tweaks" >> $LOG
+log-yakt ""
+log-yakt "Applying Ram Tweaks"
 sleep 0.5
-echo 40 > $VM/vfs_cache_pressure
-echo 15 > $VM/stat_interval
-echo 0 > $VM/compaction_proactiveness
-echo 0 > $VM/page-cluster
-echo 100 > $VM/swappiness
-echo -e "[$(date "+%H:%M:%S")] Applied Ram Tweaks\n" >> $LOG
+write "$VM/vfs_cache_pressure" 40
+write "$VM/stat_interval" 15
+write "$VM/compaction_proactiveness" 0
+write "$VM/page-cluster" 0
+write "$VM/swappiness" 100
+log-yakt "Applied Ram Tweaks"
+log-yakt ""
 
 # Mglru
-# Credits to Arter97
-echo "[$(date "+%H:%M:%S")] Cheking if your kernel has mglru support..." >> $LOG
-if [ -d $MG ]; then
-    echo "[$(date "+%H:%M:%S")] Found it." >> $LOG
-    echo "[$(date "+%H:%M:%S")] Tweaking it..." >> $LOG
-    echo 5000 > $MG/min_ttl_ms
-    echo -e "[$(date "+%H:%M:%S")] Done.\n" >> $LOG
+log-yakt ""
+log-yakt "Checking if your kernel has mglru support..."
+if [ -d "$MG" ]; then
+    log-yakt "Found it."
+    log-yakt "Tweaking it..."
+    write "$MG/min_ttl_ms" 5000
+    log-yakt "Done."
+    log-yakt ""
 else
-    echo "[$(date "+%H:%M:%S")] Your kernel doesn't support mglru :(" >> $LOG
-    echo "[$(date "+%H:%M:%S")] Aborting it..." >> $LOG
-    echo -e "[$(date "+%H:%M:%S")] Done.\n" >> $LOG
+    log-yakt "Your kernel doesn't support mglru :("
+    log-yakt "Aborting it..."
+    log-yakt ""
 fi
 
 # Set kernel.perf_cpu_time_max_percent to 40
-echo "[$(date "+%H:%M:%S")] Applying tweak for perf_cpu_time_max_percent" >> $LOG
-echo 40 > $KL/perf_cpu_time_max_percent
-echo -e "[$(date "+%H:%M:%S")] Done.\n" >> $LOG
+log-yakt ""
+log-yakt "Applying tweak for perf_cpu_time_max_percent"
+write "$KL/perf_cpu_time_max_percent" 40
+log-yakt "Done."
+log-yakt ""
 
 # Disable some scheduler logs/stats
-# Also iostats & reduce latency
-# Credits to tytydraco
-echo "[$(date "+%H:%M:%S")] Disabling some scheduler logs/stats" >> $LOG
-if [ -e $KL/sched_schedstats ]; then
-    echo 0 > $KL/sched_schedstats
+log-yakt ""
+log-yakt "Disabling some scheduler logs/stats"
+if [ -e "$KL/sched_schedstats" ]; then
+    write "$KL/sched_schedstats" 0
 fi
-echo "0	0 0 0" > $KL/printk
-echo off > $KL/printk_devkmsg
+write "$KL/printk" "0        0 0 0"
+write "$KL/printk_devkmsg" "off"
 for queue in /sys/block/*/queue
 do
-    echo 0 > "$queue/iostats"
-    echo 256 > "$queue/nr_requests"
+    write "$queue/iostats" 0
+    write "$queue/nr_requests" 256
 done
-echo -e "[$(date "+%H:%M:%S")] Done.\n" >> $LOG
+log-yakt "Done."
+log-yakt ""
 
 # Disable Timer migration
-echo "[$(date "+%H:%M:%S")] Disabling Timer Migration" >> $LOG
-echo 0 > $KL/timer_migration
-echo -e "[$(date "+%H:%M:%S")] Done.\n" >> $LOG
+log-yakt ""
+log-yakt "Disabling Timer Migration"
+write "$KL/timer_migration" 0
+log-yakt "Done."
+log-yakt ""
 
 # Cgroup Tweak
 sleep 0.5
-if [ -e $TP ]; then
+if [ -e "$TP" ]; then
     # Uclamp Tweak
-    # All credits to @darkhz
-    echo "[$(date "+%H:%M:%S")] You have uclamp scheduler" >> $LOG
-    echo "[$(date "+%H:%M:%S")] Applying tweaks for it..." >> $LOG
+    log-yakt ""
+    log-yakt "You have uclamp scheduler"
+    log-yakt "Applying tweaks for it..."
     sleep 0.3
-    for ta in $CP/*/top-app
+    for ta in "$CP"/top-app
     do
-        echo max > "$ta/uclamp.max"
-        echo 10 > "$ta/uclamp.min"
-        echo 1 > "$ta/uclamp.boosted"
-        echo 1 > "$ta/uclamp.latency_sensitive"
+        write "$ta/uclamp.max" max
+        write "$ta/uclamp.min" 10
+        write "$ta/uclamp.boosted" 1
+        write "$ta/uclamp.latency_sensitive" 1
     done
-    for fd in $CP/*/foreground
+    for fd in "$CP"/foreground
     do
-        echo 50 > "$fd/uclamp.max"
-        echo 0 > "$fd/uclamp.min"
-        echo 0 > "$fd/uclamp.boosted"
-        echo 0 > "$fd/uclamp.latency_sensitive"
+        write "$fd/uclamp.max" 50
+        write "$fd/uclamp.min" 0
+        write "$fd/uclamp.boosted" 0
+        write "$fd/uclamp.latency_sensitive" 0
     done
-    for bd in $CP/*/background
+    for bd in "$CP"/background
     do
-        echo max > "$bd/uclamp.max"
-        echo 20 > "$bd/uclamp.min"
-        echo 0 > "$bd/uclamp.boosted"
-        echo 0 > "$bd/uclamp.latency_sensitive"
+        write "$bd/uclamp.max" max
+        write "$bd/uclamp.min" 20
+        write "$bd/uclamp.boosted" 0
+        write "$bd/uclamp.latency_sensitive" 0
     done
-    for sb in $CP/*/system-background
+    for sb in "$CP"/system-background
     do
-        echo 40 > "$sb/uclamp.max"
-        echo 0 > "$sb/uclamp.min"
-        echo 0 > "$sb/uclamp.boosted"
-        echo 0 > "$sb/uclamp.latency_sensitive"
+        write "$sb/uclamp.max" 40
+        write "$sb/uclamp.min" 0
+        write "$sb/uclamp.boosted" 0
+        write "$sb/uclamp.latency_sensitive" 0
     done
     sysctl -w kernel.sched_util_clamp_min_rt_default=0
     sysctl -w kernel.sched_util_clamp_min=128
-    echo -e "[$(date "+%H:%M:%S")] Done,\n" >> $LOG
+    log-yakt "Done,"
+    log-yakt ""
 fi
 
 # Enable ECN negotiation by default
-# By kdrag0n
-echo "[$(date "+%H:%M:%S")] Enabling ECN negotiation..." >> $LOG
-echo 1 > /proc/sys/net/ipv4/tcp_ecn
-echo -e "[$(date "+%H:%M:%S")] Done.\n" >> $LOG
+log-yakt ""
+log-yakt "Enabling ECN negotiation..."
+write "/proc/sys/net/ipv4/tcp_ecn" 1
+log-yakt "Done."
+log-yakt ""
 
 # Always allow sched boosting on top-app tasks
-# Credits to tytydraco
-echo "[$(date "+%H:%M:%S")] Always allow sched boosting on top-app tasks" >> $LOG
-echo 0 > $KL/sched_min_task_util_for_colocation
-echo -e "[$(date "+%H:%M:%S")] Done.\n" >> $LOG
+log-yakt ""
+log-yakt "Always allow sched boosting on top-app tasks"
+write "$KL/sched_min_task_util_for_colocation" 0
+log-yakt "Done."
+log-yakt ""
+
 
 # Watermark Boost Tweak
-if [ -e $WT ]; then
-    echo "[$(date "+%H:%M:%S")] Disabling watermark boost..." >> $LOG
-    echo 0 > $VM/watermark_boost_factor
-    echo -e "[$(date "+%H:%M:%S")] Done.\n" >> $LOG
+if [ -e "$WT" ]; then
+    log-yakt ""
+    log-yakt "Disabling watermark boost..."
+    write "$VM/watermark_boost_factor" 0
+    log-yakt "Done."
+    log-yakt ""
 fi
 
-echo "[$(date "+%H:%M:%S")] Tweaking read_ahead overall..." >> $LOG
+log-yakt ""
+log-yakt "Tweaking read_ahead overall..."
 for queue2 in /sys/block/*/queue/read_ahead_kb
 do
-echo 128 > $queue2
+    write "$queue2" 128
 done
-echo -e "[$(date "+%H:%M:%S")] Tweaked read_ahead.\n" >> $LOG
+log-yakt "Tweaked read_ahead."
+log-yakt ""
 
 # UFSTW (UFS Turbo Write Tweak)
-echo "[$(date "+%H:%M:%S")] Checking if your kernel has UFS Turbo Write Support" >> $LOG
-if [ -e /sys/devices/platform/soc/$BT/ufstw_lu0/tw_enable ]; then
-    echo "[$(date "+%H:%M:%S")] Your kernel has UFS Turbo Write Support. Tweaking it..." >> $LOG
-    echo 1 > /sys/devices/platform/soc/$BT/ufstw_lu0/tw_enable
-    echo -e "[$(date "+%H:%M:%S")] Done.\n" >> $LOG
+log-yakt ""
+log-yakt "Checking if your kernel has UFS Turbo Write Support"
+if [ -e "/sys/devices/platform/soc/$BT/ufstw_lu0/tw_enable" ]; then
+    log-yakt "Your kernel has UFS Turbo Write Support. Tweaking it..."
+    write "/sys/devices/platform/soc/$BT/ufstw_lu0/tw_enable" 1
+    log-yakt "Done."
+    log-yakt ""
 else
-    echo -e "[$(date "+%H:%M:%S")] Your kernel doesn't have UFS Turbo Write Support.\n" >> $LOG
+    log-yakt "Your kernel doesn't have UFS Turbo Write Support."
+    log-yakt ""
 fi
 
 # Extfrag
-# Credits to @tytydraco
-echo "[$(date "+%H:%M:%S")] Increasing fragmentation index..." >> $LOG
-echo 750 > $VM/extfrag_threshold
+log-yakt ""
+log-yakt "Increasing fragmentation index..."
+write "$VM/extfrag_threshold" 750
 sleep 0.5
-echo -e "[$(date "+%H:%M:%S")] Done.\n" >> $LOG
+log-yakt "Done."
+log-yakt ""
 
 # Disable Spi CRC
-if [ -d $ML/mmc_core ]; then
-    echo "[$(date "+%H:%M:%S")] Disabling Spi CRC" >> $LOG
-    echo 0 > $ML/mmc_core/parameters/use_spi_crc
-    echo -e "[$(date "+%H:%M:%S")] Done.\n" >> $LOG
+if [ -d "$ML/mmc_core" ]; then
+    log-yakt ""
+    log-yakt "Disabling Spi CRC"
+    write "$ML/mmc_core/parameters/use_spi_crc" 0
+    log-yakt "Done."
+    log-yakt ""
 fi
 
 # Zswap Tweak
-echo "[$(date "+%H:%M:%S")] Checking if your kernel supports zswap.." >> $LOG
-if [ -d $ML/zswap ]; then
-    echo "[$(date "+%H:%M:%S")] Your kernel supports zswap, tweaking it.." >> $LOG
-    echo lz4 > $ML/zswap/parameters/compressor
-    echo "[$(date "+%H:%M:%S")] Setted your zswap compressor to lz4 (Fastest compressor)." >> $LOG
-    echo zsmalloc > $ML/zswap/parameters/zpool
-    echo -e "[$(date "+%H:%M:%S")] Setted your zpool compressor to zsmalloc." >> $LOG
-    echo -e "[$(date "+%H:%M:%S")] Tweaked!\n" >> $LOG
+log-yakt ""
+log-yakt "Checking if your kernel supports zswap.."
+if [ -d "$ML/zswap" ]; then
+    log-yakt "Your kernel supports zswap, tweaking it.."
+    write "$ML/zswap/parameters/compressor" lz4
+    log-yakt "Set your zswap compressor to lz4 (Fastest compressor)."
+    write "$ML/zswap/parameters/zpool" zsmalloc
+    log-yakt "Set your zpool compressor to zsmalloc."
+    log-yakt "Tweaked!"
+    log-yakt ""
 else
-    echo -e "[$(date "+%H:%M:%S")] Your kernel doesn't support zswap, aborting it...\n" >> $LOG
+    log-yakt "Your kernel doesn't support zswap, aborting it..."
+    log-yakt ""
 fi
 
 # Enable Power Efficient
-echo "[$(date "+%H:%M:%S")] Enabling Power Efficient..." >> $LOG
-echo 1 > $ML/workqueue/parameters/power_efficient
-echo -e "[$(date "+%H:%M:%S")] Done.\n" >> $LOG
+log-yakt ""
+log-yakt "Enabling Power Efficient..."
+write "$ML/workqueue/parameters/power_efficient" 1
+log-yakt "Done."
+log-yakt ""
+log-yakt ""
 
-echo "[$(date "+%H:%M:%S")] The Tweak is done enjoy :)" >> $LOG
+log-yakt "The Tweak is done enjoy :)"
