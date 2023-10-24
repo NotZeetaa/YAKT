ffd1a266ae84f2052b1016dff4a6c7bc9334f057
NotZeetaa <rodrigo2005contente@gmail.com>
2023-10-24 19:53:56 +0100

    tree: v12 Release
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- build.sh
+++ build.sh
@@ -9,5 +9,5 @@ curl -F document=@$1 "https://api.telegram.org/bot${token}/sendDocument" \
 }
 echo ""
 rm -rf *.zip
-zip -r9 "YAKT-v11.zip" . -x *build* -x *changelog* -x *.bak* -x *.git*
-push "YAKT-v11.zip"
+zip -r9 "YAKT-v12.zip" . -x *build* -x *changelog* -x *.bak* -x *.git*
+push "YAKT-v12.zip"
