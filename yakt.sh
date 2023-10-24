ebf2780dc6d25d521f22224375c2e4766be821bc
NotZeetaa <rodrigo2005contente@gmail.com>
2023-10-24 19:39:51 +0100

    yakt: Increase sched_nr_migrate
    * To increase scheduler performance in cost of latency
    
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- yakt.sh
+++ yakt.sh
@@ -95,7 +95,7 @@ log-yakt ""
 log-yakt "Tweaking scheduler to reduce latency"
 write "$KL/sched_migration_cost_ns" 5000000
 write "$KL/sched_latency_ns" "$SCHED_PERIOD"
-write "$KL/sched_nr_migrate" 16
+write "$KL/sched_nr_migrate" 32
 sleep 0.5
 log-yakt "Done."
 log-yakt ""
