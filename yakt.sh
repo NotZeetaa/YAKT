bfcb1a2948f4d7b7d0c446cf813371f132eb05b5
CRANKV2 <97807862+CRANKV2@users.noreply.github.com>
2023-10-25 21:32:55 +0200

    yakt: Changed log path
    - Updated the LOG and ERROR_LOG paths to /data/YAKT/yakt.log and /data/YAKT/yakt-logging-error.log, respectively.
    
    - Ensured that the necessary log functions still work correctly after the path change.
    
    This change is made to improve the log storage location for the Yakt v12 script.
    
    Contributor: @CRANKV2

--- yakt.sh
+++ yakt.sh
@@ -7,13 +7,12 @@ sleep 30
 # Log create
 log-yakt() {
     local message="$1"
-    echo "[$(date "+%H:%M:%S")] $message" >> /data/adb/modules/YAKT/yakt.log
-}
+    echo "[$(date "+%H:%M:%S")] $message" >> /data/YAKT/yakt.log
 
 # Function to log error messages
 log-error() {
     local message="$1"
-    echo "[$(date "+%H:%M:S")] $message" >> /data/adb/modules/YAKT/yakt-logging-error.log
+    echo "[$(date "+%H:%M:S")] $message" >> /data/YAKT/yakt-logging-error.log
 }
 
 write() {
@@ -39,8 +38,8 @@ write() {
 }
 
 # Modify the paths for logs
-LOG=/data/adb/modules/YAKT/yakt.log
-ERROR_LOG=/data/adb/modules/YAKT/yakt-logging-error.log
+LOG=/data/YAKT/yakt.log
+ERROR_LOG=/data/YAKT/yakt-logging-error.log
 
 if [ -f "$LOG" ]; then
     rm "$LOG"
