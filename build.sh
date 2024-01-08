9b43797e3fbe58aae4ac901afdfe4bbc0967c549
Konstantin <konstantinkreutz@gmail.com>
2024-01-07 19:15:51 -0800

    improve build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
-clog=`cat changelog.txt`
+clog=$(cat changelog.txt)
 function push() {
-curl -F document=@$1 "https://api.telegram.org/bot${token}/sendDocument" \
+curl -F document="@$1" "https://api.telegram.org/bot${token}/sendDocument" \
      -F chat_id="${chat_id}"  \
      -F "disable_web_page_preview=true" \
      -F "parse_mode=html" \
      -F caption="${clog}"
 }
 echo ""
-rm -rf *.zip
-zip -r9 "YAKT-v14.zip" . -x *build* -x *changelog* -x *.bak* -x *.git*
+rm -rf ./*.zip
+zip -r9 "YAKT-v14.zip" . -x "*build*" "*changelog*" "*.bak*" "*.git*"
 push "YAKT-v14.zip"
